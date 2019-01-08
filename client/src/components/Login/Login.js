183e3ef468d6ef74b9ed885f4018c9d23f519ea9
Radsauce <radley.eakle@yahoo.com>
2019-01-07 21:52:43 -0800

    Login.js WIP

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Applied from '../Applied'
+import Home from '../Home'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+import JobPost from '../JobPost'
+// import Login from '../Login'
+import Profile from '../Profile'
 
 class Login extends Component {
 
   render() {
     return (
       <>
-        <h1>Login</h1>
+      <Router>
+        <div>
+          <Navbar />
+          
+          <Footer />       
+        </div>
+      </Router>
       </>
     )
   }
@@ -13,3 +27,24 @@ class Login extends Component {
 
 
 export default Login
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//       <Router>
+//         <div>
+//             <Navbar />
+//             <Navbar />
+//             <Route exact path='/' component={() => <Home />} />
+//             <Route path='/login' component={() => <Login />} />
+//             <Route path='/profile' component={() => <Profile />} />
+//             <Route path='/jobpost' component={() => <JobPost />} />
+//             <Route path='/applied' component={() => <Applied />} />
+//             <Footer />
+//         </div>
+//       </Router>
+//       </>
+//     )
+//   }
+// }
