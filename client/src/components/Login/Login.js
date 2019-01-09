f44fe0450793dd4eb7609be354dacc861ae92d0e
Radsauce <radley.eakle@yahoo.com>
2019-01-08 19:06:32 -0800

    login.js works

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+// import ApplicantModal from './ApplicantModal'
+// import EmployerModal from './EmployerModal'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -6,6 +8,12 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const styles = {
   card: {
@@ -46,4 +54,8 @@ class Login extends Component {
 }
 
 
-export default Login
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
