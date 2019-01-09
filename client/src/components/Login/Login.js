3b3313764bdcf7b8123c8897d9feddef81cb4db8
Radsauce <radley.eakle@yahoo.com>
2019-01-08 19:00:36 -0800

    add logos and login WIP

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,50 +1,49 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Applied from '../Applied'
-import Home from '../Home'
-import Navbar from '../Navbar'
-import Footer from '../Footer'
-import JobPost from '../JobPost'
-// import Login from '../Login'
-import Profile from '../Profile'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 275,
+    display: "flex"
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
 
 class Login extends Component {
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <>
-      <Router>
-        <div>
-          <Navbar />
-          
-          <Footer />       
-        </div>
-      </Router>
-      </>
-    )
-  }
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Login
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                  Register
+                </Typography>
+              </CardContent>
+            </Card>
+          )
+    }
 }
 
 
 export default Login
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <>
-//       <Router>
-//         <div>
-//             <Navbar />
-//             <Navbar />
-//             <Route exact path='/' component={() => <Home />} />
-//             <Route path='/login' component={() => <Login />} />
-//             <Route path='/profile' component={() => <Profile />} />
-//             <Route path='/jobpost' component={() => <JobPost />} />
-//             <Route path='/applied' component={() => <Applied />} />
-//             <Footer />
-//         </div>
-//       </Router>
-//       </>
-//     )
-//   }
-// }
