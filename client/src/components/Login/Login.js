6eced07d560b4d2f1e5693c13feb3d85994438fc
Garrett Fermo <garrettcfermo@gmail.com>
2019-01-10 20:16:38 -0800

    updated login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-// import ApplicantModal from './ApplicantModal'
-// import EmployerModal from './EmployerModal'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import firebase from 'firebase'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -47,31 +47,32 @@ class Login extends Component {
     const { classes } = this.props;
 
     return (
-            <Card className={classes.card}>
-              <CardContent>
-                <form className={classes.container} noValidate autoComplete="off">
-                      <Grid container spacing={24}>
-                        <Grid item xs={6}>
-                          <TextField
-                            id="standard-name"
-                            label="Username"
-                            className={classes.textField}
-                            value={this.state.name}
-                            onChange={this.handleChange('name')}
-                            margin="normal"
-                          />
-                          </Grid>
-                        <Grid item xs={6}>
-                          <TextField
-                            id="standard-name"
-                            label="Password"
-                            className={classes.textField}
-                            value={this.state.name}
-                            onChange={this.handleChange('name')}
-                            margin="normal"
-                          />
-                          </Grid>
-                      </Grid>
+      <>
+        <Card className={classes.card}>
+          <CardContent>
+            <form className={classes.container} noValidate autoComplete="off">
+            <Grid container spacing={24}>
+              <Grid item xs={6}>
+                <TextField
+                  id="standard-name"
+                  label="Username"
+                  className={classes.textField}
+                  value={this.state.name}
+                  onChange={this.handleChange('name')}
+                  margin="normal"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  id="standard-name"
+                  label="Password"
+                  className={classes.textField}
+                  value={this.state.name}
+                  onChange={this.handleChange('name')}
+                  margin="normal"
+                />
+              </Grid>
+            </Grid>
             <Grid container spacing={24}>
               <Grid item xs={6}>
                 <TextField
